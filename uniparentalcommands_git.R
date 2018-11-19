5b97f49fc4dbb586fb43b737517d5b23a6323f55
chiarabarbieri <barbieri.chiara@gmail.com>
2018-11-20 00:35:54 +0100

    Update uniparentalcommands_git.R

--- uniparentalcommands_git.R
+++ uniparentalcommands_git.R
@@ -6,7 +6,7 @@ https://haplogrep.uibk.ac.at/
 # Y chromosome: extract a .vcf file with the Y chromosome variants and run it with Yhaplo 
   https://github.com/23andMe/yhaplo
 
-### uniparental visualization
+### uniparental visualization: pie charts on a map (Figure S7)
 
 # info file with meta information for each population
 info<-read.table("/Users/chiarabarbieri/Desktop/SNP_humOriginsAmericas/listpopInfo.txt", header=T, as.is=T,comment.char = "")
@@ -58,3 +58,79 @@ ggsave("MapPieChart_mtDNA_all2.pdf", useDingbats=FALSE)
 columnshg<-c("Q","C_", "othersY") #Y chromosome
 
 ggsave("MapPieChart_Ych_all.pdf", useDingbats=FALSE)
+
+
+#---------------------------------------
+### proportion of non-Native ancestry in autosomal, mt and Ych (Figure S8)
+#---------------------------------------
+admixprop<-read.table("admixtureProportionsInd.txt", header=T, as.is=T, comment.char = "") # this file comes from the supervised ADMIXTURE run elaborated, it looks like this
+
+----------
+sample_ID	pop1 Africa	Europe	Native
+HGDP00702	Piapoco	0.00001	0.00001	0.99998
+HGDP00704	Piapoco	0.00001	0.00001	0.99998
+HGDP00706	Piapoco	0.00001	0.00001	0.99998
+HGDP00920	Yoruba	0.99998	0.00001	0.00001
+HGDP00924	Yoruba	0.99998	0.00001	0.00001
+---------
+
+# mtDNA proportion per population
+femaleNonNative<-c()
+for (i in 1:nrow(mtfreq)){
+  femaleNonNative[i]<-(mtfreq$others[i]/sum(mtfreq[i,]))
+}
+
+# Ych proportion per population
+
+maleNonNative<-c()
+for (i in 1:nrow(Yfreq)){
+  maleNonNative[i]<-(Yfreq$othersY[i]/sum(Yfreq[i,]))
+}
+
+# Autosome mean proportion per population
+
+autosomeNonNative<-c()
+for (i in 1:nrow(Yfreq)){
+  temp<-autsomeAdmix[which(autsomeAdmix$population==rownames(mtfreq)[i]),]
+  autosomeNonNative[i]<-mean(temp$Africa+temp$Europe)
+}
+
+
+tabNonNativeProportion<-cbind(femaleNonNative,maleNonNative,autosomeNonNative)
+rownames(tabNonNativeProportion)<-rownames(mtfreq)
+library(reshape)
+meltNativeProportion<-melt(tabNonNativeProportion) # melt dataset for ggplot
+colnames(meltNativeProportion)<-c("population","geneticMarker","proportionNonNativeAncestry")
+
+meltNativeProportion$alphaexternal<-c(rep(0.7,25),rep(0.5,25),rep(0.9,25))
+
+info<-read.table("listpopInfo.txt", header=T, as.is=T,comment.char = "") #extra info, one line per population. includes the grouping scheme.
+infoRED<-info[which(info$population%in%rownames(tabNonNativeProportion)),]
+poporder<-as.character(infoRED$population)
+
+
+coso<-merge(meltNativeProportion,infoRED, by = "population",  all.x = TRUE)
+
+
+#trick to order the color patterns according to group populations belong to
+nomibelli<-as.character(unique(coso$group))
+riordinacolori<-as.character(unique(coso$group))
+listpopInfo2<-infoRED
+listpopInfo2<-listpopInfo2[!duplicated(listpopInfo2$group),]
+rownames(listpopInfo2)<-listpopInfo2$group
+listpopInfo2<-listpopInfo2[riordinacolori,]
+colorelli<-as.character(listpopInfo2$color)
+
+
+ggplot(coso, aes(geneticMarker, proportionNonNativeAncestry, fill=group,color=group,alpha=alphaexternal)) +   
+    geom_bar( stat="identity") +    
+  theme(axis.text.x = element_text(angle = 45, vjust=1, hjust=1)) +
+  theme(text = element_text(size=10))+
+  scale_fill_manual(values = colorelli,
+                    breaks=nomibelli,
+                    name="Source Geographic Group")+
+  scale_color_manual(values = colorelli,
+                     breaks=nomibelli,
+                     name="Source Geographic Group")+
+  facet_wrap(~population)
+ggsave("NONNativeAncestryProportion_3markers.pdf", useDingbats=FALSE)
